6aa11501631f56ac4fa20a1b33a3c61f747a887c
Vinícius <vinicius.cs01>
2017-07-17 20:14:30 -0300

    Commit Inicial

---
+++ XF.Recursos/XF.Recursos/Estilo/GeralView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Recursos.Estilo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GeralView : ContentPage
+    {
+        public GeralView()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGlobal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.GeralView());
+        }
+    }
+}
